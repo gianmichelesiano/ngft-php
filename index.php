adacc99cbca03c5ee5e18c86e91e7c395c4c1872
Gianmichele Siano <gianmichele.siano@gmail.com>
2022-02-02 14:06:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php 
 # use PHP Version 8.1.2”;
 function bagsclassification($input, $limitClassS = 20, $limitClassM = 30 , $limitClassL= 50 , $limitClassXL = 70) {
@@ -9,29 +5,30 @@ function bagsclassification($input, $limitClassS = 20, $limitClassM = 30 , $limi
   $info = "The class for the weight <b>{$input}</b>  is: "; 
   
   if ($weight <= 0) {
-      echo "{$info} weight could be > 0 <br>";
+     $retVal =   "{$info} weight could be > 0 <br>";
   } elseif (($weight > 0 ) && ($weight <= $limitClassS )){
-      echo "{$info} Class S <br>";
+      $retVal = "{$info} Class S <br>";
   } elseif (($weight > $limitClassS ) && ($weight <=  $limitClassM )){
-      echo "{$info} Class M <br>";
+      $retVal = "{$info} Class M <br>";
   } elseif (($weight >  $limitClassM ) && ($weight <= $limitClassL )){
-      echo "{$info} Class L <br>";
+      $retVal = "{$info} Class L <br>";
   } elseif (($weight > $limitClassL ) && ($weight <= $limitClassXL )){
-      echo "{$info} Class XL <br>";
+      $retVal = "{$info} Class XL <br>";
   } elseif ($weight > $limitClassXL){
-      echo "{$info} Not allowed to be shipped by air travel <br>";
+      $retVal = "{$info} Not allowed to be shipped by air travel <br>";
   } else {
-      echo "{$info} Not right value for bags weight";
+      $retVal = "{$info} Not right value for bags weight";
   }
+  return $retVal;
 }
 
-bagsclassification(-2);
-bagsclassification(12);
-bagsclassification(23);
-bagsclassification(44);
-bagsclassification(70);
-bagsclassification(75);
-bagsclassification('test');
+echo(bagsclassification(-2));
+echo(bagsclassification(12));
+echo(bagsclassification(23));
+echo(bagsclassification(40));
+echo(bagsclassification(75));
+echo(bagsclassification("test"));
+
 ?>
 
 </body>
